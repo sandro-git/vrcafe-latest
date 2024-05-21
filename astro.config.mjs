a482f6104529bd8abc8fa56b130cfa1f78992901
Sandro Das Neves <17152022+sandro-git@users.noreply.github.com>
2024-05-21 12:40:19 +0200

    add kayak to partners

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,8 @@ import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
+  image: {
+    remotePatterns: [{ protocol: "https" }],
+  }
   integrations: [react(), tailwind(), icon()]
 });
