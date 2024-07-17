eb8982d786d91921200cd3cd57a843302ab63722
Sandro Das Neves <17152022+sandro-git@users.noreply.github.com>
2024-07-18 00:13:19 +0200

    add sitemap and robots.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,20 @@
 import { defineConfig } from "astro/config";
+
 import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
-
 import react from "@astrojs/react";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://vr-cafe.fr",
   image: {
-    remotePatterns: [{
-      protocol: "https"
-    }]
+    remotePatterns: [
+      {
+        protocol: "https",
+      },
+    ],
   },
-  integrations: [tailwind(), icon(), react()]
+  integrations: [tailwind(), icon(), react(), sitemap()],
 });
