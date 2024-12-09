5e7eab4ec98f6c8e335e521a77662d9d40640b9e
Sandro Das Neves <17152022+sandro-git@users.noreply.github.com>
2024-12-09 19:53:41 +0100

    Refactor contact page and update Astro configuration. Removed form handling logic from the contact page and adjusted the Astro config to set output to 'static'. Cleaned up integration formatting for better readability.

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,26 +3,29 @@ import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
 import react from "@astrojs/react";
 import sitemap from "@astrojs/sitemap";
-
 import sanity from "@sanity/astro";
-
 import vue from "@astrojs/vue";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://vr-cafe.fr",
+  output: 'static',
   image: {
     remotePatterns: [{
       protocol: "https"
     }]
   },
-  integrations: [tailwind(), icon(), react(), sitemap(), sanity(
-    {
+  integrations: [
+    tailwind(),
+    icon(),
+    react(),
+    sitemap(),
+    sanity({
       projectId: "7ef57i1i",
       dataset: "production",
       useCdn: false,
       studioBasePath: '/admin',
-    }
-  ), vue()],
-  output: 'hybrid'
+    }),
+    vue()
+  ]
 });
