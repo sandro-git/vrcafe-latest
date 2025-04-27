4509eac51e68f464a094f0c2d121321d44e8b7df
Sandro <17152022+sandro-git@users.noreply.github.com>
2025-04-27 16:30:33 +0200

    Refactor code structure for improved readability and maintainability

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,12 @@ import icon from "astro-icon";
 import react from "@astrojs/react";
 import sitemap from "@astrojs/sitemap";
 
+import netlify from "@astrojs/netlify";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://vr-cafe.fr",
   integrations: [tailwind(), icon(), react(), sitemap()],
+  output: "server",
+  adapter: netlify(),
 });
